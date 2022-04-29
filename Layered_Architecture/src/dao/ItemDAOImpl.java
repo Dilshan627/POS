0693de7c724a313917aa6c57eb105e23abfba6cf
Dilshan <73770222+Caseynik@users.noreply.github.com>
2022-04-29 10:40:27 +0530

    saveItem Refactored

--- Layered_Architecture/src/dao/ItemDAOImpl.java
+++ Layered_Architecture/src/dao/ItemDAOImpl.java
@@ -25,6 +25,16 @@ public class ItemDAOImpl {
         return allItem;
     }
 
+    public boolean saveItem(ItemDTO itemDTO) throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        PreparedStatement pstm = connection.prepareStatement("INSERT INTO Item (code, description, unitPrice, qtyOnHand) VALUES (?,?,?,?)");
+        pstm.setString(1, itemDTO.getCode());
+        pstm.setString(2, itemDTO.getDescription());
+        pstm.setBigDecimal(3, itemDTO.getUnitPrice());
+        pstm.setInt(4, itemDTO.getQtyOnHand());
+        return pstm.executeUpdate() > 0;
+    }
+
     public boolean deleteItem(String code) throws SQLException, ClassNotFoundException {
         Connection connection = DBConnection.getDbConnection().getConnection();
         PreparedStatement pstm = connection.prepareStatement("DELETE FROM Item WHERE code=?");
