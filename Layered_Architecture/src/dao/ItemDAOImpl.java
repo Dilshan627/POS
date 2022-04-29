322b35b1933ea6bc9e6b6d28985a6f501b38c396
Dilshan <73770222+Caseynik@users.noreply.github.com>
2022-04-29 10:26:36 +0530

    ItemDAOImpl created and updateItem Refactored

--- Layered_Architecture/src/dao/ItemDAOImpl.java
+++ Layered_Architecture/src/dao/ItemDAOImpl.java
@@ -32,5 +32,14 @@ public class ItemDAOImpl {
         return pstm.executeUpdate() > 0;
     }
 
+    public boolean updateItem(ItemDTO itemDTO) throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        PreparedStatement pstm = connection.prepareStatement("UPDATE Item SET description=?, unitPrice=?, qtyOnHand=? WHERE code=?");
+        pstm.setString(1, itemDTO.getDescription());
+        pstm.setBigDecimal(2, itemDTO.getUnitPrice());
+        pstm.setInt(3, itemDTO.getQtyOnHand());
+        pstm.setString(4, itemDTO.getCode());
+        return pstm.executeUpdate() > 0;
+    }
 
 }
